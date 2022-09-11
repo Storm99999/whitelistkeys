abf0351767979aae01ca386aa4fc23678a2548b4
Storm <87811650+Storm99999@users.noreply.github.com>
2022-09-11 22:15:14 +0200

    Update sw.lite.loader.lua

--- lite/sw.lite.loader.lua
+++ lite/sw.lite.loader.lua
@@ -1,6 +1,8 @@
 _G.lockMeth = "Random" -- Change to head if you want your silent aim to ONLY aim at the head.
 _G.silMeth = "FPORWIL" -- Silent Aim Method, You're gonna have to play around with it. The Default one is FindPartOnRayWithIgnoreList but shortened. 
-_G.AntiFly = true -- Change if the game you're playing has an Anti-Fly, set it to false, if it has, set it to true, if it doesnt.
+_G.AntiFly = false -- Change if the game you're playing has an Anti-Fly, set it to false, if it has, set it to true, if it doesnt.
+_G.Keystrokes = true -- If you want sexy keystrokes, turn it on.
+_G.HideOverlay = false -- If you want the StormWare Watermark along with the features you've toggled shown, set it to true, not recommended for closet cheating, obviously.
 
 
 local g = game:HttpGet("https://raw.githubusercontent.com/Storm99999/whitelistkeys/main/lite/sw.lite.lua")
