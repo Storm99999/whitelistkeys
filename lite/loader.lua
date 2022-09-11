688e66530ba3157105695e683390fc076c36eef1
Storm <87811650+Storm99999@users.noreply.github.com>
2022-09-11 12:45:49 +0200

    Update loader.lua

--- lite/loader.lua
+++ lite/loader.lua
@@ -1,11 +1,3 @@
 local target = "https://raw.githubusercontent.com/Storm99999/whitelistkeys/main/lite/sw.lite.lua"
 
-LoadFunction(target, true)
-
-function LoadFunction(script, isProtected)
-  if isProtected == true then
-    loadstring(game:HttpGet(script:reverse()))()
-   else
-    loadstring(game:HttpGet(script))() -- ok
-    end
-end
+loadstring(game:HttpGet(target:reverse()))()
