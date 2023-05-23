d68821b4e37623ac711a60854231234d5e30a3ca
Storm <87811650+Storm99999@users.noreply.github.com>
2023-05-23 22:40:04 +0200

    Update tenacity_library.lua

--- bedwars/tenacity_library.lua
+++ bedwars/tenacity_library.lua
@@ -782,9 +782,6 @@ function library:CreateDropdown(items, constant, frame)
     local object = game:GetObjects("rbxassetid://13481020329")[1]
     object.Parent = library.Settings[frame]:FindFirstChild('main').main2.Items;
     object.dropFrame.dropScroll.AutomaticCanvasSize = "Y"
-    object.dropFrame.dropScroll.BorderSizePixel = 0
-    object.dropFrame.dropScroll.ScrollBarThickness = 3
-    object.dropFrame.dropScroll.ScrollBarImageColor3 = Color3.fromRGB(255,255,255)
     local Sample = object.dropFrame.dropScroll.Sample:Clone()
     object.name.Text = items[1];
     object.dropFrame.ZIndex = 9
